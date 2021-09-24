b014c055e63006871789c6f1e733b01d7a3434d4
yyq <yyq.work@foxmail.com>
2021-09-24 23:40:10 +0800

    完成对剪切板变化的监听

---
+++ 剪切板自动备份/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace 剪切板自动备份
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("楷体", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (134)));
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(343, 56);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "剪切板自动备份工具";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipText = "剪切板小工具";
+            this.notifyIcon1.BalloonTipTitle = "小工具";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon) (resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(34, 74);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(288, 92);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 172);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(367, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(131, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(367, 194);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int) (((byte) (128)))), ((int) (((byte) (64)))), ((int) (((byte) (0)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WinCopBak";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+
+        private System.Windows.Forms.Label label1;
+
+        #endregion
+    }
+}
---
+++ 剪切板自动备份/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 剪切板自动备份
+{
+    public partial class Form1 : Form
+    {
+        private readonly string _path = @"e:/x/" + DateTime.Now.ToString("yyyy-MM-dd");
+        
+        public Form1()
+        {
+            InitializeComponent();
+            AddClipboardFormatListener(Handle);
+            CheckFileExists();
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            notifyIcon1.Visible = false;
+            Visible = true;
+            ShowInTaskbar = true;
+        }
+
+        private const int WM_SYSCOMMAND = 0x112;
+        // const int SC_CLOSE = 0xF060;
+        private const int SC_MINIMIZE = 0xF020;
+        private const int WM_DRAWCLIPBOARD = 0x031D;
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_SYSCOMMAND)
+            {
+                if (m.WParam.ToInt32() == SC_MINIMIZE)
+                {
+                    ShowInTaskbar = false;
+                    Visible = false;
+                    notifyIcon1.Visible = true;
+                    return;
+                }
+            }
+
+            if (m.Msg == WM_DRAWCLIPBOARD)
+            {
+                //显示剪贴板中的文本信息
+                if (Clipboard.ContainsText())
+                {
+                    toolStripStatusLabel1.Text = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "==>捕捉到文本";
+                    SendNote(Clipboard.GetText());
+                }
+
+                //显示剪贴板中的图片信息
+                if (Clipboard.ContainsImage())
+                {
+                    pictureBox1.Image = Clipboard.GetImage();
+                    pictureBox1.Update();
+                    pictureBox1.Image.Save(_path + "/" + DateTime.Now.ToString("HHmmss") + ".jpg");
+                    toolStripStatusLabel1.Text = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "==>捕捉到截图";
+                }
+            }
+
+            base.WndProc(ref m);
+        }
+
+        #region MyRegion
+
+        //API declarations...
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr AddClipboardFormatListener(IntPtr hwnd);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern bool RemoveClipboardFormatListener(IntPtr hWnd);
+
+        #endregion
+
+        // 窗口关闭前移除对剪切板监视
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            RemoveClipboardFormatListener(Handle);
+        }
+
+        // 查看文件夹是否存在
+        private void CheckFileExists()
+        {
+            if (!Directory.Exists(_path))
+            {
+                Directory.CreateDirectory(_path);
+            }
+        }
+
+        private void SendNote(string content)
+        {
+            var bw = new BinaryWriter(new FileStream(_path + "/bak.yyq", FileMode.Append));
+            var br = new BinaryReader(new FileStream(_path + "/bak.yyq", FileMode.Open, FileAccess.Read, FileShare.ReadWrite));
+            
+            bw.Write("\n" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "--->" + content);
+            toolStripStatusLabel1.Text = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            bw.Close();
+
+            var readString = br.ReadString();
+            toolStripStatusLabel1.Text = readString;
+            br.Close();
+        }
+    }
+}
